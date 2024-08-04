973fbb02e27f9b3a5c1527299caed58c889a84c2
Juan Lee <ninejuan@protonmail.com>
2024-08-04 04:02:38 +0000

    add: Database Link

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { linkToDatabase } from './utils/db.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await linkToDatabase();
   await app.listen(3000);
 }
 bootstrap();
