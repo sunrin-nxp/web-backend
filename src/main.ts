3c85213a375eb1de4985bd6ab51aae445a837443
Juan Lee <ninejuan@protonmail.com>
2024-08-04 04:05:23 +0000

    add: winston Logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { linkToDatabase } from './utils/db.util';
+import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(winstonLogger);
   await linkToDatabase();
   await app.listen(3000);
 }
