74b934e7d79fb98fe7b0a0e4d11aacd43c210f8a
Juan Lee <ninejuan@protonmail.com>
2024-08-05 05:57:19 +0000

    add: user schema, interface <- wrong problems Array

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,11 +1,12 @@
 interface User {
     nxpid: String;
-    nxppw: String;
-    description: String;
-    associated: String;
+    nxppw?: String;
+    description?: String;
+    associated?: String;
     mailaddr: String;
     rank: String;
-    solved_problems: Array<String>;
+    solved_problems?: Array<String>;
+    wrong_problems?: Array<String>;
 };
 
 export default User;
