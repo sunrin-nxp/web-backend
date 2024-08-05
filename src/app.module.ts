3d71eec50107f625d10103f22bc75a3974962f68
Juan Lee <ninejuan@protonmail.com>
2024-08-05 05:34:52 +0000

    add: User res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middleware/winston.middleware';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
