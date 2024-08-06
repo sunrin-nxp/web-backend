03e4b736db97a1d7b5e4beb3e93ada2fe33e69ee
Juan Lee <ninejuan@protonmail.com>
2024-08-06 00:27:24 +0000

    add: Auth Service <- Auth provider setting

--- src/res/auth/auth.module.ts
+++ src/res/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './utils/LocalStrategy.auth.util';
+import { SessionSerializer } from './utils/Serializer.auth.util';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    SessionSerializer,
+    {
+      provide: 'AUTH_SERVICE',
+      useClass: AuthService
+    }
+  ],
 })
 export class AuthModule {}
