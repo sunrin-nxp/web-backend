0d0989fab9de8bdeb633693bc99103ef6d0bdf2c
Juan Lee <ninejuan@protonmail.com>
2024-08-07 06:45:17 +0000

    refactor: user login service rewrite

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -3,6 +3,7 @@ import mongo from 'mongoose';
 const userSchema = new mongo.Schema({
     nxpid: { type: String, required: true },
     nxppw: { type: String, required: true }, // bcrypt 이용한 암호화 필요
+    nickname: { type: String, required: true },
     description: { type: String, default: "" },
     associated: { type: String, default: "" },
     mailaddr: { type: String, required: true },
--- src/res/auth/auth.service.ts
+++ src/res/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import userSchema from 'src/models/user.schema';
 import User from 'src/interface/user.interface';
 import { config } from 'dotenv'; config();
@@ -9,37 +9,64 @@ const env = process.env;
 
 @Injectable()
 export class AuthService {
-  constructor() { }
-  // LocalStrategy.auth.util.ts에서 참조
-  async validateUser(details: User) {
-    const user = await userSchema.findOne({
-      nxpid: details.nxpid
-    });
-    if (user) return user; else return null;
-  }
+	constructor() { }
 
-  async findUser(userId: Number) {
-    const user = await userSchema.findOne({ nxpid: userId });
-    if (user) return user; else return null;
-  }
+	async login(
+		id: string,
+		pw: string,
+	): Promise<{
+		accessToken: string;
+		refreshToken: string;
+	}> {
+		const user = await this.validateUser(id, pw);
+		if (!user) {
+			throw new HttpException("올바르지 않은 정보입니다.", 400);
+		}
+		const payload = { id: user.id, nick: user.nickname };
+		const accessToken = this.jwtService.sign(payload, {
+			secret: env.ACCESS_TOKEN_SECRET
+		});
+		const refreshToken = this.jwtService.sign(payload, {
+			secret: env.REFRESH_TOKEN_SECRET,
+			expiresIn: '7d'
+		});
+		await this.setCurrentRefreshToken(refreshToken, user.id);
+		return {
+			accessToken: accessToken,
+			refreshToken: refreshToken
+		}
+	}
+	
+	// LocalStrategy.auth.util.ts에서 참조
+	async validateUser(details: User) {
+		const user = await userSchema.findOne({
+			nxpid: details.nxpid
+		});
+		if (user) return user; else return null;
+	}
 
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+	async findUser(userId: Number) {
+		const user = await userSchema.findOne({ nxpid: userId });
+		if (user) return user; else return null;
+	}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+	create(createAuthDto: CreateAuthDto) {
+		return 'This action adds a new auth';
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+	findAll() {
+		return `This action returns all auth`;
+	}
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} auth`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+	update(id: number, updateAuthDto: UpdateAuthDto) {
+		return `This action updates a #${id} auth`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} auth`;
+	}
 }
