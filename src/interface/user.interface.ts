96b854987fe426b24cbf2ba9a0cdc6f3ad86be6c
Juan Lee <ninejuan@protonmail.com>
2024-08-07 07:04:50 +0000

    update: user interface

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,12 +1,16 @@
 interface User {
     nxpid: String;
     nxppw?: String;
+    nickname?: String;
     description?: String;
     associated?: String;
     mailaddr?: String;
+    profilePhoto?: String;
     rank?: String;
-    solved_problems?: Array<String>;
-    wrong_problems?: Array<String>;
+    solved_problems?: Array<Number>;
+    wrong_problems?: Array<Number>;
+    my_problems?: Array<Number>;
+    contributed_problems?: Array<Number>;
 };
 
 export default User;
