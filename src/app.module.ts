80629874fcc3e7fc27cc9d82d766f65dee97a2b5
Juan Lee <ninejuan@protonmail.com>
2024-08-13 18:03:15 +0000

    add: problem schema, interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middleware/winston.middleware';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './res/auth/auth.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
