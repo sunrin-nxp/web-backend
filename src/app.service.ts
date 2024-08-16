509c4d18de9900df14702c4b4387c2770f2d624c
Juan Lee <ninejuan@protonmail.com>
2024-08-16 12:40:46 +0000

    test: main page mock data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'; 
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async mainPage() {
+    return {
+      banner: {
+        background_color: ""  ,
+        boxText: "",
+        title: "",
+        content: "",
+        imgUrl: ""
+      },
+      problems: [{ // 제일 최신 문제 50개 반환
+
+      }],
+      streak: "",
+      dailyQuest: [
+        {
+          title: "생일 케이크 만들기",
+          rank: "Silver3",
+          solved: true
+        },
+        {
+          title: "선린냥이지기",
+          rank: "Platinum1",
+          solved: false
+        },
+        {
+          title: "비가 주륵 내리고 나면",
+          rank: "Master",
+          solved: false
+        }
+      ]
+    };
   }
 }
